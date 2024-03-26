f0c6e182ffdc546aa584b953eed565f3acea374f
prof <spasmodic51@gmail.com>
2024-03-26 19:39:49 +0100

    Maquette avec loader terminée, code js à entamer

---
+++ main.js
@@ -0,0 +1,14 @@
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        console.log(`Latitude: ${lat}, longitude: ${lng}`);
+      },
+      (error) => {
+        console.error("Error getting user location:", error);
+      }
+    );
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
